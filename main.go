e8e21987f5307e7681148a0f2ba9b95c26228696
LAPTOP-KC535EQA\User <jiachwen99@gmail.com>
2023-06-28 22:26:21 +0800

    Solve linting issue

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,7 +15,13 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		_, err := w.Write([]byte("Hello World!"))
+		if err != nil {
+			log.Println("Error Warning : ", err)
+		}
 	})
-	http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":3000", router)
+	if err != nil {
+		log.Println("Fail Server : ", err)
+	}
 }
